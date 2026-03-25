a24f529d2768909cceaa45be1416f811b36be9ba
devolina <devolina52@gmail.com>
2026-03-25 11:09:24 +0530

    latest commit 01

--- src/demo/Demo1.java
+++ src/demo/Demo1.java
@@ -1,7 +1,22 @@
 package demo;
 
+import org.testng.annotations.Test;
+
 public class Demo1 {
 
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+		System.out.println("postjira2");
+		System.out.println("postjira3");
+		System.out.println("postJira4");
+		System.out.println("postJira5");
+		System.out.println("postJira6");
+		System.out.println("postJira7");
+		System.out.println("postJira8");
+		System.out.println("postJira9");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
